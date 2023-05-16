69347589bfc16eafacde277ea31400c11e26f4eb
Horus Sinhue Camacho <62359674+horuscamacho@users.noreply.github.com>
2023-05-15 21:48:57 -0600

    Terminando la generación del PDF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'cv-generator';
+/*  @ViewChild('content', { static: false }) el!: ElementRef<HTMLImageElement>;
+  @ViewChild('contentTwo', { static: false }) el2!: ElementRef<HTMLImageElement>;*/
+  constructor(
+  ) {
+  }
+  @ViewChild('content', { static: false }) el!: ElementRef<HTMLSelectElement>;
+  element: any;
+
+  ngOnInit(){
+    this.element = document.getElementById('content');
+  }
+
+  generatePDF(){
+    console.log(this.element)
+  let resume = window.open(this.element, '_blank');
+    const close = () => {
+      setTimeout(() => {
+        resume?.close();
+      }, 1000)
+    }
+  resume?.focus();
+  resume?.print();
+  close()
+
+
+
+  }
+
+
+
+
+
 }
